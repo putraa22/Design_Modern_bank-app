2f1b0ad8689ead04efd71bb2ea6b392f865d6842
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-08-24 20:45:29 +0700

    Add Components

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import styles from "./style";
+
+import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from "./components";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overlay-hidden">
-      <h1>Hello World</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}  `}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients /> <CTA /> <Footer />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ bank_modern_app/src/components/Business.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bussines = () => {
+  return <div>Bussines</div>;
+};
+
+export default Bussines;
