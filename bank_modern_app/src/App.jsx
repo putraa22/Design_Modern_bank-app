0e0b15aa042e46e775f5121423314e0d7192cc57
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-08-24 20:17:36 +0700

    First Commit

---
+++ bank_modern_app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overlay-hidden">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
